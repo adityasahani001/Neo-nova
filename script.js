4f5da8730c5024a291337d3624a90dbde25ef6ce
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2025-05-10 23:52:55 +0530

    script.js

--- script.js
+++ script.js
@@ -77,6 +77,8 @@ document.addEventListener("DOMContentLoaded", () => {
     return returnElement ? messageDiv : null;
   }
 
+
+
   function convertMarkdownToHTML(markdown) {
     return markdown
       .replace(/^### (.*$)/gim, '<h3>$1</h3>')
