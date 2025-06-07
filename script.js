87df83aad153ce15384012e1dd18c6b7c4a4c757
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2025-06-07 23:30:18 +0530

    script.js

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error("Search error:", error);
         displayMessage("⚠ Could not fetch real-time data.", "bot-message");
       }
+
     } else {
       try {
         const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY_GEMINI}`, {
